15e70f44fffa65154956fdf9aee4973a2eace5ae
Jefiozie <jefiozie.bosch@gmail.com>
2020-12-21 15:29:27 +0100

    fix: remove not needed part

--- libs/read-time/src/lib/readTime.ts
+++ libs/read-time/src/lib/readTime.ts
@@ -1,20 +1,20 @@
-import { log, registerPlugin, renderRoute } from '@scullyio/scully';
+import { registerPlugin } from '@scullyio/scully';
 import * as fs from 'fs';
 import * as readingTime from 'reading-time';
 
 export const readTimeFunc = async (routes: any) =>
   routes.map((route) => {
+    // make configurable
     if (route.route.startsWith('/articles/article')) {
       const content = fs.readFileSync(route.templateFile).toString('utf-8');
       const stats = readingTime(content);
-      const newRoute = {
+      return {
         ...route,
         data: {
           ...route.data,
           readingTime: stats.minutes > 1 ? stats.minutes : 1,
         },
-      };
-      return newRoute;
+      }
     }
     return route;
   });
