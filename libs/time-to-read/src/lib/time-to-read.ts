d07d23561606d8f9324efc874b113a5c6239dc13
Jefiozie <jefiozie.bosch@gmail.com>
2020-12-24 13:05:02 +0100

    fix(time-to-read): filter routes based on templateFile

--- libs/time-to-read/src/lib/time-to-read.ts
+++ libs/time-to-read/src/lib/time-to-read.ts
@@ -4,24 +4,26 @@ import * as readingTime from 'reading-time';
 
 export const timeToRead = 'timeToRead';
 export interface timeToReadOptions {
-  path: string
+  path: string;
 }
 export const timeToReadFunc = async (routes: HandledRoute[]) => {
   const options: timeToReadOptions = getMyConfig(timeToReadFunc);
-  return routes.map((route) => {
-    if (route.route.startsWith(options.path)) {
-      const content = fs.readFileSync(route.templateFile).toString('utf-8');
-      const stats = readingTime(content);
-      const newRoute = {
-        ...route,
-        data: {
-          ...route.data,
-          readingTime: stats.minutes > 1 ? stats.minutes : 1,
-        },
-      };
-      return newRoute;
-    }
-    return route;
-  });
-}
+  return routes
+    .filter((route) => route.templateFile)
+    .map((route) => {
+      if (route.route.startsWith(options.path)) {
+        const content = fs.readFileSync(route.templateFile).toString('utf-8');
+        const stats = readingTime(content);
+        const newRoute = {
+          ...route,
+          data: {
+            ...route.data,
+            readingTime: stats.minutes > 1 ? stats.minutes : 1,
+          },
+        };
+        return newRoute;
+      }
+      return route;
+    });
+};
 registerPlugin('routeProcess', timeToRead, timeToReadFunc);
